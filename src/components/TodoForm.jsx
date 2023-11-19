c00529f6431315766333171700cd2bce7e62b4b2
Carllos166 <carlos_rj18@hotmail.com>
2023-11-19 18:46:50 -0300

    reset values

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,20 +1,40 @@
-import PropTypes from 'prop-types';
-
+import { useState } from "react";
 
 const TodoForm = () => {
-  return <div className="todo-form">
-    <h2>Criar tarefa</h2>
-    <form action="">
-        <input type="text" placeholder='Digite o título' />
-        <select>
-            <option value="">Selecione uma categoria</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Pessoal">Pessoal</option>
-            <option value="Estudos">Estudos</option>
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+    // adicionar todo
+    setValue("");
+    setCategory("");
+  };
+
+  return (
+    <div className="todo-form">
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o título"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
         </select>
         <button type="submit">Criar tarefa</button>
-    </form>
-  </div>
-}
+      </form>
+    </div>
+  );
+};
 
-export default TodoForm
+export default TodoForm;
