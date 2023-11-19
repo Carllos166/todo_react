c8932533279a33dc40d838fdc221b5b90e1b409a
Carllos166 <carlos_rj18@hotmail.com>
2023-11-19 18:10:27 -0300

    Criando um form e adicionando CSS

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,11 +2,19 @@ import PropTypes from 'prop-types';
 
 
 const TodoForm = () => {
-  return (
-    <div>
-      TodoForm
-    </div>
-  )
+  return <div className="todo-form">
+    <h2>Criar tarefa</h2>
+    <form action="">
+        <input type="text" placeholder='Digite o título' />
+        <select>
+            <option value="">Selecione uma categoria</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar tarefa</button>
+    </form>
+  </div>
 }
 
 export default TodoForm
